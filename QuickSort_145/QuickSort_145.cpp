8bbd29eced05ce97e50562422bff41a899639ffe
nrismawardah <nrismawardah@gmail.com>
2023-03-30 15:51:16 +0700

    Membuat prosedur display

--- QuickSort_145/QuickSort_145.cpp
+++ QuickSort_145/QuickSort_145.cpp
@@ -94,6 +94,21 @@ void q_short(int low, int high)
 
     // Sort the list on the right of pivot using quick sort
     q_short(j + 1, high);                                       // Langkah Algoritma No.13
+
+}
+
+void display() {
+    cout << "\n-----------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparisions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
 }
 
 
