9fb33499e4ca97fbd8c0136cb3724c4a57db5e46
nrismawardah <nrismawardah@gmail.com>
2023-03-30 15:02:22 +0700

    Add project files.

---
+++ QuickSort_145/QuickSort_145.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];            // Array of integers to hold values
+int cmp_count = 0;      // Number of comparison
+int mov_count = 0;      // Number of data movements
+int n;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
