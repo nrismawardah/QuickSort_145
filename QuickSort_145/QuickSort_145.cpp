96994797a02cf4f6d62b0bb96ace09a52465236e
nrismawardah <nrismawardah@gmail.com>
2023-03-30 15:25:32 +0700

    Membuat langkah algoritma no.2

--- QuickSort_145/QuickSort_145.cpp
+++ QuickSort_145/QuickSort_145.cpp
@@ -43,12 +43,14 @@ void swap(int x, int y)
 void q_short(int low, int high)
 {
     int pivot, i, j;
-    if (low > high)             // Langkah Algoritma No.1
+    if (low > high)                                             // Langkah Algoritma No.1
         return;
 
     // Partition the list into two parts:
     // One containing elements less that or equal to pivot
     // Outher containing elements greater than pivot
+
+    pivot = arr[low];                                           // Langkah Algoritma No.2
 }
 
 
