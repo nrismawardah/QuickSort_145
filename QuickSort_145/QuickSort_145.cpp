acd846e00362e4231bc58da0150f865c80f6ea05
nrismawardah <nrismawardah@gmail.com>
2023-03-30 15:44:53 +0700

    Membuat langkah algoritma no.13

--- QuickSort_145/QuickSort_145.cpp
+++ QuickSort_145/QuickSort_145.cpp
@@ -91,6 +91,9 @@ void q_short(int low, int high)
     }
     // Sort the list on the left of pivot using quick sort
     q_short(low, j - 1);                                        // Langkah Algoritma No.12
+
+    // Sort the list on the right of pivot using quick sort
+    q_short(j + 1, high);                                       // Langkah Algoritma No.13
 }
 
 
